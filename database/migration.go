9ed835e34b4bef2d8203d4e79cbeb16a432b23a6
Yaşar Ünyılmaz <116540999+yasarunylmzz@users.noreply.github.com>
2024-12-27 12:58:40 +0300

    routes and handlers

--- database/migration.go
+++ database/migration.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"database/sql"
+	"log"
+)
+
+
+func migrate(db *sql.DB){
+
+	createTableSQL := 
+	`
+	CREATE TABLE IF NOT EXISTS user {
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		email TEXT,
+		name TEXT,
+		surname TEXT,
+		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+	}
+	`
+	_, err := db.Exec(createTableSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
